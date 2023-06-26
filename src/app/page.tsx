ded1af828afe57908e3d6cd3fdaa6bf8d249dd92
Ertan Özdemir <ertan_kubi@hotmail.com>
2023-06-26 15:14:27 +0300

    Add main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import NewUserForm from "@/components/NewUserForm.tsx/NewUserForm";
+import styles from "./styles.module.css";
+
 export default function Home() {
-  return <main>Hello World!</main>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <span className={styles["header--bold"]}>JS</span>
+        <span className={styles["header--light"]}>Articles.dev</span>
+        <hr className={styles.horizontal} />
+      </div>
+
+      <p className={styles.paragraph}>
+        Unleash the power of JavaScript with our weekly newsletter! Dive into
+        handpicked articles by top experts in the field. Stay up-to-date with
+        trends, tips, and tricks to elevate your coding skills. Don't miss out
+        on exciting JavaScript content
+        <span className={styles["paragraph--bold"]}>—join us now!</span>
+      </p>
+
+      <NewUserForm />
+    </div>
+  );
 }
---
+++ src/components/NewUserForm.tsx/NewUserForm.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React, { useState } from "react";
+import styles from "./styles.module.css";
+
+const validateEmail = (email: string) => {
+  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+  return emailRegex.test(email);
+};
+
+export default function NewUserForm() {
+  const [email, setEmail] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+
+  const handleEmailChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setError(null);
+    const { value } = event.target;
+    setEmail(value);
+  };
+
+  const handleClick = () => {
+    const isValid = validateEmail(email);
+    if (isValid) {
+    } else {
+      setError("This e-mail address is not valid!");
+    }
+  };
+
+  return (
+    <div className={styles.form__container}>
+      <div className={styles.box__error}>{error}</div>
+      <input
+        className={styles.form__input}
+        onChange={handleEmailChange}
+        placeholder='Enter your e-mail'
+      />
+      <button className={styles.form__button} onClick={handleClick}>
+        Join Now
+      </button>
+      <div>
+        <div className={styles.box}>
+          <span className={styles.box__link}>Let me see first</span>
+        </div>
+      </div>
+    </div>
+  );
+}

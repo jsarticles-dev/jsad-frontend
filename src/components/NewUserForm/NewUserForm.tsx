2d00b3b4bff94d51f97f7014493542e19431f030
Ertan Özdemir <ertan_kubi@hotmail.com>
2023-08-18 12:18:00 +0300

    Fix

--- src/components/NewUserForm/NewUserForm.tsx
+++ src/components/NewUserForm/NewUserForm.tsx
@@ -27,9 +27,26 @@ export default function NewUserForm({
     setEmail(value);
   };
 
-  const handleClick = () => {
+  const handleClick = async () => {
     const isValid = validateEmail(email);
     if (isValid) {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
+        method: "POST",
+        cache: "no-store",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      if (!res.ok) {
+        const response: any = await res.json();
+        if (response?.message) {
+          return toastMessage.error(response.message);
+        }
+        throw new Error("Failed to fetch data");
+      }
+
       toastMessage.success("Successfully joined!");
     } else {
       setError("This e-mail address is not valid!");
